46bb09157a0ad0e97b466f418f92c02caadac736
root6439 <nicolas.soaresh.98@gmail.com>
2019-07-04 22:56:25 -0300

    Criação da classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,7 +11,11 @@ public class Program {
 
 		Department obj = new Department(1, "livros");
 		
+		Seller seller = new Seller(1, "Nicolas", "nicolas@gmail.com", new Date(), 3000.00, obj);
+		
 		System.out.println(obj);
+		
+		System.out.println(seller);
 	}
 
 }
